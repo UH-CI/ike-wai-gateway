14813a4d143f42ab72c59fbaa09f9b12250806f3
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2016-11-01 23:08:50 -0600

    Add Filemetadata Controller, view
    Add Filemetadata Controller and manage view

---
+++ app/js/controllers/filemetadata/FileMetadataController.js
@@ -0,0 +1,44 @@
+angular.module('AgaveToGo').controller('FileMetadataController', function ($scope, $state, $stateParams, $translate, MetaController, FilesController, ActionsService, MessageService) {
+    $scope._COLLECTION_NAME = 'filemetadata';
+    $scope._RESOURCE_NAME = 'filemetadatum';
+
+    $scope.sortType = 'name';
+    $scope.sortReverse  = true;
+
+    $scope.query = '{"associationIds":"' +  $stateParams.uuid + '"}';
+    //$scope.query ="{'associationIds':'673572511630299622-242ac113-0001-002'}";
+  //  $scope.query["associationIds"] = $stateParams.uuid;
+
+    $scope.filemetadatumUuid = $stateParams.uuid;
+
+    $scope.refresh = function() {
+      $scope.requesting = true;
+
+      MetaController.listMetadata($scope.query,null,0).then(
+          function (response) {
+            $scope.totalItems = response.result.length;
+            $scope.pagesTotal = Math.ceil(response.result.length / $scope.limit);
+            $scope[$scope._COLLECTION_NAME] = response.result;
+            $scope.requesting = false;
+          },
+          function(response){
+            MessageService.handle(response, $translate.instant('error_filemetadata_list'));
+            $scope.requesting = false;
+          }
+      );
+
+    };
+
+    $scope.searchTools = function(query){
+      $scope.query = query;
+      $scope.refresh();
+    }
+
+
+    $scope.refresh();
+
+    $scope.confirmAction = function(resourceType, resource, resourceAction, resourceList, resourceIndex){
+      ActionsService.confirmAction(resourceType, resource, resourceAction, resourceList, resourceIndex);
+    }
+
+});
