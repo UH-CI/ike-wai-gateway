0e777c952017268edca130498e88e54aba176047
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2017-02-12 16:37:23 -0700

    add broadcast reload response of stagged files
    add broadcast reload response of stagged files

--- app/js/controllers/stagged/StaggedController.js
+++ app/js/controllers/stagged/StaggedController.js
@@ -27,7 +27,7 @@ angular.module('AgaveToGo').controller('StaggedController', function($scope, $st
     FilesMetadataService.rejectStaggingRequest("484964208339784166-242ac1110-0001-012", fileUuid).then(function(result){
         $scope.metadatum = null;
         //pause to let model update
-        $timeout(function(){$scope.getMetadatum()}, 500);
+        $timeout(function(){$scope.getMetadatum()}, 300);
         $scope.requesting = false;
       });
     }
@@ -35,11 +35,16 @@ angular.module('AgaveToGo').controller('StaggedController', function($scope, $st
   $scope.publishStaggedFile = function(fileUuid, filepath){
     $scope.requesting = true;
     FilesMetadataService.publishStaggedFile(fileUuid, filepath).then(function(result){
-
+       //pause to let model update
+       $timeout(function(){$scope.getMetadatum()}, 300);
         $scope.requesting = false;
     })
   }
+  $scope.$on('broadcastUpdate', function(event, args){
+    $scope.getMetadatum();
+    alert(angular.toJson(args))
+    App.alert(args);
+  });
 
   $scope.getMetadatum();
-
 });
