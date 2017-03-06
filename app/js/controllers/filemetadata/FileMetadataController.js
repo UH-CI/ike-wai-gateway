f2f775981798323d8bcce651b854ee80aa90a85c
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2017-03-06 09:40:52 -1000

    create a default File metadata object to add associations to
    create a default File metadata object to add associations to vs modify
    other metadata objects

--- app/js/controllers/filemetadata/FileMetadataController.js
+++ app/js/controllers/filemetadata/FileMetadataController.js
@@ -1,4 +1,4 @@
-angular.module('AgaveToGo').controller('FileMetadataController', function ($scope, $state, $stateParams, $translate, $timeout, $window, MetaController, FilesController, FilesMetadataService, ActionsService, MessageService) {
+angular.module('AgaveToGo').controller('FileMetadataController', function ($scope, $state, $stateParams, $translate, $timeout, $window, MetaController, FilesController, FilesMetadataService, ActionsService, MessageService,MetadataService) {
     $scope._COLLECTION_NAME = 'filemetadata';
     $scope._RESOURCE_NAME = 'filemetadatum';
 
@@ -9,7 +9,7 @@ angular.module('AgaveToGo').controller('FileMetadataController', function ($scop
     $scope.ignoreMetadataType = ['published','stagged','PublishedFile','rejected'];
     //Don't display metadata schema types as options
     $scope.ignoreSchemaType = ['PublishedFile'];
-    
+
     $scope.query = '{"associationIds":"' +  $stateParams.uuid + '"}';
     $scope.schemaQuery ='';//"{'owner':'seanbc'}";
     //$scope.query ="{'associationIds':'673572511630299622-242ac113-0001-002'}";
@@ -19,14 +19,27 @@ angular.module('AgaveToGo').controller('FileMetadataController', function ($scop
 
     $scope.refresh = function() {
       $scope.requesting = true;
-
       MetaController.listMetadataSchema(
 				$scope.schemaQuery
 			).then(function(response){
 				$scope.metadataschema = response.result;
 				$scope.requesting = false;
 			})
+      //check if default filemetadata object exists
+      MetaController.listMetadata("{$and:[{'name':'File'},{'associationIds':['"+$stateParams.uuid+"']}]}").then(
 
+        function (response) {
+          $scope.fileMetadataObject = response.result;
+          if ($scope.fileMetadataObject == ""){
+            //we have no object so create a new on
+            $scope.createFileObject($stateParams.uuid);
+          }
+        },
+        function(response){
+          MessageService.handle(response, $translate.instant('error_filemetadata_list'));
+          $scope.requesting = false;
+        }
+      )
       MetaController.listMetadata($scope.query,null,0).then(
           function (response) {
             $scope.totalItems = response.result.length;
@@ -71,6 +84,32 @@ angular.module('AgaveToGo').controller('FileMetadataController', function ($scop
       });
     }
     }
+
+    $scope.createFileObject = function(fileUuid){
+      var body={};
+      //associate system file with this metadata File object
+      body.associationIds = [fileUuid];
+      body.name = 'File';
+      body.value = {};
+      //File Schema uuid
+      body.schemaId = '3557207775540866585-242ac1110-0001-013';
+      MetaController.addMetadata(body)
+        .then(
+          function(response){
+            $scope.metadataUuid = response.result.uuid;
+            App.alert({message: $translate.instant('File is ready for adding metadata') });
+            //add the default permissions for the system in addition to the owners
+            MetadataService.addDefaultPermissions($scope.metadataUuid);
+            $scope.requesting = false;
+            $scope.refresh();
+          },
+          function(response){
+            MessageService.handle(response, $translate.instant('error_metadata_add'));
+            $scope.requesting = false;
+          }
+        );
+      }
+
 
 
 
