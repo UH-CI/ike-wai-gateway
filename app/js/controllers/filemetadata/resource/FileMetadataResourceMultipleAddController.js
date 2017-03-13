b09c08378e5465f05075ce37a8e7c489fca099da
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2017-03-12 22:23:23 -1000

    update multiple file metadata creation to use new File metadata object
    update multiple file metadata creation to use new File metadata object

--- app/js/controllers/filemetadata/resource/FileMetadataResourceMultipleAddController.js
+++ app/js/controllers/filemetadata/resource/FileMetadataResourceMultipleAddController.js
@@ -1,20 +1,18 @@
 angular.module('AgaveToGo').controller("FileMetadataResourceMultipleAddController", function($scope, $state, $stateParams, $translate, $window, WizardHandler, MetaController, FilesController, MetadataService, ActionsService, MessageService) {
 	$scope.model = {};
-		if ($stateParams.associatedUuid){
-			$scope.model.associatedUuid = $stateParams.uuid;
-		}
+
 		$scope.fileUuids = $stateParams['fileUuids'];
+		$scope.filePaths = $stateParams['filePaths'];
 		$scope.filename = $stateParams['filename'];
-		//$scope.schemauuid = $stateParams.schemauuid;
-		/*if ($stateParams.metadataschemaUuid){
-			$scope.model.resource = $stateParams.resourceType;
-		}*/
+		$scope.schemauuid = $stateParams.schemauuid;
+
 
     //$scope.query="{'uuid': //'"+$scope.schemauuid+"'}"//"{'uuid':'316750742996381210-242ac1110-0001-013'}";
     $scope.schemaQuery ='';//"{'owner':'seanbc'}";
 
 		$scope.fetchMetadataSchema = function(schemauuid) {
 			$scope.requesting = true;
+			if (schemauuid) {
 			MetaController.getMetadataSchema(schemauuid)
 				.then(
 					function(response){
@@ -34,6 +32,10 @@ angular.module('AgaveToGo').controller("FileMetadataResourceMultipleAddControlle
 						$scope.requesting = false;
 					}
 			);
+		  }
+		  else{
+				$scope.requesting = false;
+			}
 		}
 
     $scope.refresh = function() {
@@ -50,17 +52,65 @@ angular.module('AgaveToGo').controller("FileMetadataResourceMultipleAddControlle
 					$scope.fetchMetadataSchema($stateParams.schemauuid);
 			}
 
+			//fetch File metadata objects
+			MetaController.listMetadata("{$and:[{'name':'File'},{'associationIds':{$in :['"+$scope.fileUuids.join("','")+"']}}]}").then(
+
+        function (response) {
+          $scope.fileMetadataObjects = response.result;
+					if ($scope.fileMetadataObjects.length < $scope.fileUuids.length){
+            //we have object mistmatch so figure our which are missing
+						angular.forEach($scope.fileUuids, function(value, key) {
+							MetaController.listMetadata("{$and:[{'name':'File'},{'associationIds':'"+value+"'}]}").then(
+								function(resp){
+									$scope.fileobj = resp.result;
+									if ($scope.fileobj == ""){
+										$scope.createFileObject(value);
+									}
+							  }
+  						)
+						})
+						//objects should be created fetch them all now
+						MetaController.listMetadata("{$and:[{'name':'File'},{'associationIds':{$in :['"+$stateParams.fileUuids.join("','")+"']}}]}").then(
+							function(resp){
+						  	$scope.fileMetadataObjects = resp.result;
+							}
+						)
+          }
+					else{
+						//do nothing
+					}
+				}
+			)
     };
 
     $scope.refresh();
 
 
 
-	  $scope.fetchFile = FilesController.listFileItems("{'uuid':'"+$scope.uuid+"'}").then(
-			function(response){
-				$scope.file = response.result;
-			}
-		);
+		$scope.createFileObject = function(fileUuid){
+      var body={};
+      //associate system file with this metadata File object
+      body.associationIds = [fileUuid];
+      body.name = 'File';
+      body.value = {};
+      //File Schema uuid
+      body.schemaId = '3557207775540866585-242ac1110-0001-013';
+      MetaController.addMetadata(body)
+        .then(
+          function(response){
+            $scope.metadataUuid = response.result.uuid;
+            App.alert({message: $translate.instant('File is ready for adding metadata') });
+            //add the default permissions for the system in addition to the owners
+            MetadataService.addDefaultPermissions($scope.metadataUuid);
+            $scope.requesting = false;
+            $scope.refresh();
+          },
+          function(response){
+            MessageService.handle(response, $translate.instant('error_metadata_add'));
+            $scope.requesting = false;
+          }
+        );
+      }
 		//$scope.submit = function(){
 		$scope.onSubmit = function(form) {
 			$scope.requesting = true;
@@ -79,7 +129,9 @@ angular.module('AgaveToGo').controller("FileMetadataResourceMultipleAddControlle
 							$scope.metadataUuid = response.result.uuid;
 							//add the default permissions for the system in addition to the owners
 							MetadataService.addDefaultPermissions($scope.metadataUuid);
-							MetadataService.addAssociation($scope.fileUuids[0],$scope.metadataUuid);
+							angular.forEach($scope.fileMetadataObjects, function(value,key){
+								MetadataService.addAssociation(value.uuid,$scope.metadataUuid);
+							});
 							$scope.requesting = false;
 							//$state.go('filemetadata',{id: $scope.fileUuids[0]});
 							$window.history.back();
