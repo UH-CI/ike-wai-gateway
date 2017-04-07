4f5801df88b2a3d3ce0ddee9a5ecee55e1801f5b
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2017-04-06 23:07:02 -0600

    basic search
    basic search

--- app/js/main.js
+++ app/js/main.js
@@ -2294,6 +2294,34 @@ AgaveToGo.config(['$stateProvider', '$urlRouterProvider', '$urlMatcherFactoryPro
             }
         })
 
+        .state('basic-search', {
+            url: "/basic-search",
+            templateUrl: "views/search/basic-search.html",
+            data: {pageTitle: 'Search Ike'},
+            controller: "BasicSearchController",
+            resolve: {
+                deps: ['$ocLazyLoad', function($ocLazyLoad) {
+                    return $ocLazyLoad.load({
+                        serie: true,
+                        name: 'AgaveToGo',
+                        insertBefore: '#ng_load_plugins_before', // load the above css files before '#ng_load_plugins_before'
+                        files: [
+                            '../assets/global/scripts/datatable.js',
+                            '../bower_components/holderjs/holder.js',
+                            'js/services/ActionsService.js',
+                            'js/services/MessageService.js',
+                            'js/services/PermissionsService.js',
+                            'js/services/MetadataService.js',
+                            'js/services/FilesMetadataService.js',
+                            'js/controllers/MetadataQueryBuilderController.js',
+                            'js/controllers/search/BasicSearchController.js',
+                            'js/controllers/metadata/resource/ModalMetadataResourceDetailsController.js',
+                        ]
+                    });
+                }]
+            }
+        })
+
         /**********************************************************************/
         /**********************************************************************/
         /***                                                                ***/
