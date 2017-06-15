41fef5d5b2e79fb1d7415b0a19f4ae4ac952de1b
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2017-06-15 09:58:02 -0600

    fix datetimepicker conflict issue
    remove duplicate js files (jQuery from bower seemed to be the biggest
    culprit), move date time initialization into angular controller.

--- app/js/controllers/filemetadata/FileMetadataController.js
+++ app/js/controllers/filemetadata/FileMetadataController.js
@@ -105,7 +105,9 @@ angular.module('AgaveToGo').controller('FileMetadataController', function ($scop
       MetaController.listMetadataSchema(
         $scope.schemaQuery
       ).then(function(response){$scope.metadataschema = response.result;})
-
+      jQuery('#datetimepicker1').datetimepicker();
+      jQuery('#datetimepicker2').datetimepicker();
+      jQuery('#datetimepicker3').datetimepicker();
     };
 
     $scope.fetchMetadata = function(metadata_query){
