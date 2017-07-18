6815d4684e4cee1d0642100e44b305ccb6ff8642
Jennifer Geis <jgeis@hawaii.edu>
2017-07-17 16:42:45 -1000

    fixed things that were causing error messages in the logs

--- app/js/main.js
+++ app/js/main.js
@@ -30,7 +30,7 @@ var AgaveToGo = angular.module("AgaveToGo", [
   'ui-leaflet'
 ]).service('NotificationsService',['$rootScope', '$localStorage', 'MetaController', 'toastr', function($rootScope, $localStorage, MetaController, toastr){
     if (typeof $localStorage.tenant !== 'undefined' && typeof $localStorage.activeProfile !== 'undefined') {
-      this.client = new Fpp.Client('https://48e3f6fe.fanoutcdn.com/fpp');
+      this.client = new Fpp.Client('https://9d1e23fc.fanoutcdn.com/fpp');
       this.channel = this.client.Channel($localStorage.tenant.code + '/' + $localStorage.activeProfile.username);
       this.channel.on('data', function (data) {
         var toastData = {};
