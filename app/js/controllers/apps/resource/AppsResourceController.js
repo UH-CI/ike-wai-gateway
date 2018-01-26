8c3dacf26ebcb51870df4e68456bc693a72f1f6a
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2018-01-26 10:41:43 -0700

    hide execution system on app manager
    hide execution system on app manager

--- app/js/controllers/apps/resource/AppsResourceController.js
+++ app/js/controllers/apps/resource/AppsResourceController.js
@@ -9,15 +9,15 @@ angular.module('AgaveToGo').controller("AppsResourceController", function($scope
 		$scope.active = function(route){
 			// default to details tab
 			if ($state.current.name === "apps"){
-				$state.go("apps.details")
+				$state.go("apps.run")
 			}
 
 			return $state.is(route);
 		};
 
 		$scope.tabs = [
-			{ heading: "Details", route:"apps.details", active:false },
-			{ heading: "Run", route:"apps.run", active:false }
+			//{ heading: "Details", route:"apps.details", active:false },
+			{ heading: "Run Application", route:"apps.run", active:true }
 			// { heading: "Stats", route:"apps.stats", active:false },
 		];
 
