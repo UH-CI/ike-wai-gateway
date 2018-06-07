f6bfaa05ff258033c527cc8b5e72f9dca6210b78
Sean B. Cleveland <sean.b.cleveland@gmail.com>
2018-06-07 11:36:08 -0600

    merge

--- app/js/controllers/apps/resource/AppsResourceController.js
+++ app/js/controllers/apps/resource/AppsResourceController.js
@@ -9,38 +9,23 @@ angular.module('AgaveToGo').controller("AppsResourceController", function($scope
 		$scope.active = function(route){
 			// default to details tab
 			if ($state.current.name === "apps"){
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
 				$state.go("apps.details")
 	}
-=======
+
 				$state.go("apps.details")
 			}
->>>>>>> Stashed changes
-=======
-				$state.go("apps.details")
-			}
->>>>>>> Stashed changes
+
 
 			return $state.is(route);
 		};
 
 		$scope.tabs = [
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 
 			{ heading: "Details", route:"apps.details", active:false },
 			{ heading: "Run Application", route:"apps.run", active:false }
 
-=======
-			{ heading: "Details", route:"apps.details", active:false },
-			{ heading: "Run", route:"apps.run", active:false }
->>>>>>> Stashed changes
-=======
-			{ heading: "Details", route:"apps.details", active:false },
-			{ heading: "Run", route:"apps.run", active:false }
->>>>>>> Stashed changes
+
 			// { heading: "Stats", route:"apps.stats", active:false },
 		];
 
