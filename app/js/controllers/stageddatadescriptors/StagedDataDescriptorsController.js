8f54c9463f525c43218797206109bb080eb4c6af
Jennifer Geis <jgeis@hawaii.edu>
2020-03-02 12:21:55 -1000

    Using real Data Descriptor data rather than hard coded

--- app/js/controllers/stageddatadescriptors/StagedDataDescriptorsController.js
+++ app/js/controllers/stageddatadescriptors/StagedDataDescriptorsController.js
@@ -1,4 +1,4 @@
-angular.module('AgaveToGo').controller('StagedDataDescriptorsController', function ($scope, $state, $stateParams, $translate, $uibModal, $rootScope, $localStorage, MetaController, FilesController, ActionsService, MessageService, MetadataService) {
+angular.module('AgaveToGo').controller('StagedDataDescriptorsController', function ($scope, $http, $state, $stateParams, $translate, $uibModal, $rootScope, $localStorage, MetaController, FilesController, ActionsService, MessageService, MetadataService) {
     $scope._COLLECTION_NAME = 'metadata',
     $scope._RESOURCE_NAME = 'metadatum';
     $scope.showModal = false;
@@ -127,27 +127,196 @@ angular.module('AgaveToGo').controller('StagedDataDescriptorsController', functi
       $scope.updateDataDescriptor(dataDescriptor);
     }
 
+    $scope.getAuthors = function (dataDescriptor) {
+      /* 
+      ex: "creators":[{"first_name":"Kiana",
+      "last_name":"Frank",
+      "organization":"University of Hawaiʻi at Manoa",
+      "phone":"(808) 956-0403",
+      "email":"klfrank@hawaii.edu",
+      "uuid":"5977219466484968985-242ac1110-0001-012"},
+
+      {"first_name":"Diamond",
+      "last_name":"Tachera",
+      "organization":"University of Hawaiʻi at Manoa",
+      "email":"diamondt@hawaii.edu",
+      "uuid":"4865671556713222631-242ac1110-0001-012"}]
+      */
+      var arr = [];
+      // our "creator" is HS's "author"
+      var authors = dataDescriptor.value.creators;
+      var spacer = ", ";
+      angular.forEach(authors, function (author) {
+        if (author != undefined) {
+          // if either first or last is not set, don't use a comma
+          if (author.first_name == undefined || author.last_name == undefined) {
+            spacer = "";
+            if (author.first_name == undefined) {
+              author.first_name = "";
+            }
+            if (author.last_name == undefined) {
+              author.last_name = "";
+            }
+          }
+          arr.push(`"` + author.last_name + spacer + author.first_name + `"`);      
+        }
+      });
+      console.log("author array: " + arr);
+      var authorString = "";
+      if (arr.length > 0) {
+        authorString = `"authors": [${arr}],`;
+      }
+      return authorString;
+      //return arr;
+    }
+
+    $scope.getFromDate = function(dataDescriptor) {
+      //console.log("StagedDataDescriptorsController.getFromDate");
+      var dString = "";
+      var d = dataDescriptor.value.start_datetime;
+      //console.log("from_date: " + d);
+      if (d) {
+        dString = `"from_date": "` + d + `",`;
+      }
+      //console.log("Start Date: " + dString);
+      return dString;
+    }
+
+    $scope.getToDate = function(dataDescriptor) {
+      //console.log("StagedDataDescriptorsController.getToDate");
+      var dString = "";
+      var d = dataDescriptor.value.end_datetime;
+      //console.log("to_date: " + d);
+      if (d) {
+        dString = `"to_date": "` + d + `",`;
+      }
+      //console.log("To Date: " + dString);
+      return dString;
+    }
+
+    $scope.getAbstract = function(dataDescriptor) {
+      //console.log("StagedDataDescriptorsController.getAbstract");
+      var s = "";
+      var d = dataDescriptor.value.description;
+      //console.log("abstract: " + d);
+      if (d) {
+        // strip off newlines as it make Hydroshare choke.
+        s = `"abstract": "${d.replace(/(\r\n|\n|\r)/gm, "")}",`
+      }
+      //console.log("String: " + s);
+      return s;
+    }
+
+    $scope.getKeywords = function(dataDescriptor) {
+      /* format: "keywords": ["keyword1","keyword2"], */
+      //console.log("StagedDataDescriptorsController.getKeywords");
+      var s = "";
+      var d = dataDescriptor.value.subjects;
+      //console.log("subjects: " + JSON.stringify(d));
+      if (d) {
+        s = `"keywords": ${JSON.stringify(d)},`
+      }
+      //console.log("String: " + s);
+      return s;
+    }
+
     $scope.publishStagedDDToHydroshare = function (dataDescriptor) {
+      console.log("StagedDataDescriptorsController.publishStagedDDToHydroshare: " + dataDescriptor.uuid);
+      console.log("dataDescriptor: " + JSON.stringify(dataDescriptor));
       
       // Send the data off to Hydroshare.
       //var userInfoURL = `https://www.hydroshare.org/hsapi/userInfo/?access_token=${$scope.accessToken}&format=json`;
-      var hsURL = `${baseHSURL}/hsapi/resource/?access_token=${$scope.hydroshareAccessToken}`;
+      baseHSURL = "https://www.hydroshare.org";
 
-      // TODO: some of these have multiple entries (like creator), need to handle that
+      // g6QWYGsTM1RdNG3110oS8Li41gtXgW
+      var hsURL = `${baseHSURL}/hsapi/resource/?access_token=g6QWYGsTM1RdNG3110oS8Li41gtXgW`;
+      //var hsURL = `${baseHSURL}/hsapi/resource/?access_token=${$rootScope.hydroshareAccessToken}`;
+      console.log("hsURL: " + hsURL);
+
+      // "creator" in Hydroshare's API is called "owner" on their interface.  
+      // This is the user against whose quota the resource is counted in managing storage,
+      // so for us, creator is 'ikewai.'  Need to make a creator object there and store the
+      // reference here.  While technically, there can be multiple owners, for our case, we
+      // will be the only one.
+
+      // Meanwhile, our backend "creators" field is the match for HS's "authors" API field.
+      // we use the term 'authors' on our interface as well, just not in the JSON.
+
+      // "authors":"${dataDescriptor.creators.first_name} ${dataDescriptor.creators.last_name} ",
+
+      // trying to figure out how to get all our user info into HS for authorship.
+      // curl -X GET "https://www.hydroshare.org/hsapi/user/" -H "accept: application/json" -H "X-CSRFToken: CkpfWN0dmDKsZ3wYUlhjdIT7hyu0FCGh4XxUQlyvI537JCi1Yx9wzcmclKHJSdGz"
+      /* {
+          "username": "jgeis@hawaii.edu",
+          "email": "jgeis@hawaii.edu",
+          "first_name": "Jennifer",
+          "id": 1501,
+          "last_name": "Geis",
+          "title": "Software Engineer",
+          "organization": "University of Hawaii"
+        }
+      */
+
+      // "authors": ["${$scope.getAuthorArray(dataDescriptor)}"],
+      // "to_date": "2019-10-10",
+      // "abstract": "some abstract description",
+      //  "keywords": [
+      //    "keyword1",
+      //    "keyword2"
+      //  ],
+
+      // "creator": "ikewai",
+      // ${$scope.getAuthors(dataDescriptor)}
+      // ${$scope.getFromDate(dataDescriptor)}
+      // ${$scope.getToDate(dataDescriptor)}
+      // "availability": [ "public" ],
+
+      /* 
+      // THIS WORKS
       var hsData = `{
-          "title": "${dataDescriptor.title}",
-          "author": "Jennifer Geis",
-          "creator": "${dataDescriptor.creators.first_name} ${dataDescriptor.creators.last_name} ",
-          "from_date": "2019-10-09",
-          "to_date": "2019-10-10",
-          "abstract": "some abstract description",
-          "keywords": [
-              "keyword1",
-              "keyword2"
-          ],
-          "abstract": "abstract string",
+        "title": "some random title",
+        "authors": ["Geis, Jennifer","Cleveland, Sean"],
+        "creator": "ikewai",
+        "to_date": "2019-10-10",
+        "from_date": "2019-10-10",
+        "abstract": "some abstract description",
+        "keywords": [
+          "keyword1",
+          "keyword2"
+        ],
+        "availability": [ "public" ],
+        "resource_type": "CompositeResource"
+      }`;
+      */
+
+      /*
+      // This works, the issue was the newline in the abstract
+      var hsData = `{
+        "title": "Diamond Tachera sampling rain water at UHH CC in August 2018",
+        "creator": "ikewai",
+        "authors": ["Geis, Jennifer","Cleveland, Sean"],
+        "from_date": "2019-08-12",
+        "abstract": "Diamond Tachera, Graduate Research Assistant, Dept. of Earth Sciences, SOEST, sampling rain water at the UH Hawaii Community College Pālamanui Campus in August 2018. Photo taken by Dr. Kiana Frank, Assistant Professor, PBRC",
+        "keywords": ["sampling","rainwater","UHH","Hilo","Pālamanui","soest","pbrc","photo","ikewai"],
+        "availability": [ "public" ],
+        "resource_type": "CompositeResource"
+      }`;
+      */
+      
+      var hsData = `{
+          "title": "${dataDescriptor.value.title}",
+          "creator": "ikewai",
+          ${$scope.getAuthors(dataDescriptor)}
+          ${$scope.getFromDate(dataDescriptor)}
+          ${$scope.getToDate(dataDescriptor)}
+          ${$scope.getAbstract(dataDescriptor)}
+          ${$scope.getKeywords(dataDescriptor)}
+          "availability": [ "public" ],
           "resource_type": "CompositeResource"
       }`;
+
+      console.log("request: " + hsData);
+
       // file=@/PATH/TO/A/FILE
       // metadata = '[{"coverage":{"type":"period", "value":{"start":"01/01/2000", "end":"12/12/2010"}}}, {"creator":{"name":"John Smith"}}, {"creator":{"name":"Lisa Miller"}}]'
       // extra_metadata = '{"key-1": "value-1", "key-2": "value-2"}'
@@ -157,7 +326,7 @@ angular.module('AgaveToGo').controller('StagedDataDescriptorsController', functi
       // "view_users": set(page.view_users.all()),
       // "can_edit": (user in set(page.edit_users.all())) \
       //             or (len(set(page.edit_groups.all()).intersection(set(user.groups.all()))) > 0)
-      /*
+      
       console.log("hsURL: " + hsURL);
       $http({
           method: 'POST',
@@ -169,18 +338,21 @@ angular.module('AgaveToGo').controller('StagedDataDescriptorsController', functi
           $scope.responseData = response.data;
           //console.log("userInfo: " + $scope.userInfo);
           console.log("resource_id: " + $scope.responseData.resource_id);
-          // format:  {"username":"jgeis@hawaii.edu","first_name":"Jennifer","last_name":"Geis","title":"Software Engineer","id":1501,"organization":"University of Hawaii","email":"jgeis@hawaii.edu"}
+
+          // TODO: add the resource_id to the data descriptor!
+
+          // userInfo format:  {"username":"jgeis@hawaii.edu","first_name":"Jennifer","last_name":"Geis","title":"Software Engineer","id":1501,"organization":"University of Hawaii","email":"jgeis@hawaii.edu"}
       }, function errorCallback(response) {
           alert("HydroshareOAuthController.submitToHydroshare Error. Try Again!");
       });
 
-
+      
       //console.log("staged? " + dd.value.stagedToHydroshare);
       // mark the dd as being "pushedToHydroshare" and no longer staged.
       dataDescriptor.value.stagedToHydroshare = false;
       dataDescriptor.value.pushedToHydroshare = true;
       $scope.updateDataDescriptor(dataDescriptor);
-      */
+      
     }
 
     $scope.updateDataDescriptor = function (dataDescriptor) {
